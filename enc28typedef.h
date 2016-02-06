833225741e1e6692f6fa5305c5aa2dec159dce1b
reaper7 <reaper7md@gmail.com>
2016-02-06 20:30:43 +0100

    change CS pin for ENERGIA from 10 to 13

--- enc28typedef.h
+++ enc28typedef.h
@@ -421,7 +421,7 @@
 
 #if (ENERGIA)
 
-  #define ENC28J60_CONTROL_CS                     10
+  #define ENC28J60_CONTROL_CS                     13
   //#define SPI_MOSI				13
   //#define SPI_MISO				12
   //#define SPI_SCK					14
